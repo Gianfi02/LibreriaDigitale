0c5c32ae56efe7a3fe230942f614a952d95ca35f
Gianfi02 <gianfelice@outlook.com>
2026-06-29 10:27:28 +0200

    FASE_1, classe generica Libro creata

---
+++ src/Biblioteca.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Biblioteca {
+    private List<Libro<?>> libri;
+    private Set<String> utenti;
+    private Map<String, Stack<Libro<?>>> libri_inprestito;
+    private Queue<Libro<?>> noleggiati;
+
+}
---
+++ src/Libro.java
@@ -0,0 +1,3 @@
+public class Libro<T> {
+    private T codice;
+}
